12949039c1cef9bf3329a204969d7185777774db
root <root@ip-172-31-14-34.ap-south-1.compute.internal>
2026-03-27 11:00:19 +0000

    3rd commit

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker + Jenkins CI/CD!');
+  res.send('Hello from Docker + Jenkins CI/CD and awesome !');
 });
 
 app.listen(PORT, () => {
